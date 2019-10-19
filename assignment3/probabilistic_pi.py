d36b5a88e4bc8bfe289ae500e6012254a1074c7e
johnhshapiro <jshapir4@msudenver.edu>
2019-10-19 17:42:51 -0600

    pi is now approx pi because its definitely not pi

--- assignment3/probabilistic_pi.py
+++ assignment3/probabilistic_pi.py
@@ -10,10 +10,10 @@ def calculate_pi_probabilistically(number_of_darts):
         if ((x ** 2 + y ** 2) ** (0.5)) <= 0.5:
             darts_in_circle += 1
 
-    pi = 4 * darts_in_circle / number_of_darts
-    percent_from_pi = "{:.1}".format(abs(pi - math.pi) / math.pi)
+    approx_pi = 4 * darts_in_circle / number_of_darts
+    percent_from_pi = "{:.1}".format(abs(approx_pi - math.pi) / math.pi)
     print(f"Number of Darts: {number_of_darts}\n" +
-          f"Calculated pi: {pi}\n" +
+          f"Calculated pi: {approx_pi}\n" +
           f"Off of Accepted by {percent_from_pi}%\n" +
           "***********************")
 
