c4901adedbac95175cd6b177fa71662d80a947f8
johnhshapiro <jshapir4@msudenver.edu>
2019-11-15 13:33:07 -0700

    added init, generate random

--- assignment4/random.py
+++ assignment4/random.py
@@ -2,12 +2,16 @@ import time
 import math
 
 class RandGen():
-
+    """Class that generates a sequence of 'random' numbers
+    """
     def __init__(self):
-        self.seed = time.time()
+        self.seed = int(str(time.time())[-3:].replace('.',''))
 
     def generate_random(self):
-        digit = int(str(seed)[-1])
-        print(digit)
-        number = int(str(int(str(seed)[-1])/math.e)[1])
-        return number
+        self.seed = 7 * self.seed % 101
+        return (self.seed -1) % 10 + 1
+
+if __name__ == '__main__':
+    random = RandGen()
+    for i in range(100):
+        print(random.generate_random())
