55ecef60bb8367cda4f8c0d12ecbd621333dda11
johnhshapiro <jshapir4@msudenver.edu>
2019-11-15 12:55:11 -0700

    Mystery

--- assignment4/random.py
+++ assignment4/random.py
@@ -1,4 +1,13 @@
 import time
 import math
 
-seed = time.time()
+class RandGen():
+
+    def __init__(self):
+        self.seed = time.time()
+
+    def generate_random(self):
+        digit = int(str(seed)[-1])
+        print(digit)
+        number = int(str(int(str(seed)[-1])/math.e)[1])
+        return number
